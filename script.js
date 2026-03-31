73ba26cdf55ee89b84dfc5f18f5d638c9005cc88
walthol <96192081+walthol@users.noreply.github.com>
2026-03-31 09:01:46 -0400

    Add setup instructions for game buttons
    Add setup instructions for creating game buttons in script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+        // HOW I CAN SET UP IF I FORGET!
+        // 1. Go To "Game Buttons"
+        // 2. Create The Button. The "button id" should be the same name as the name, but with the work "Button" at the end. same setup with capitals and crap like the others
+        // 3. Go To "GAME URLS"
+        // 4. Change what is typed in for the name already to your game id, and remove the word "Button"
+        // 5. Put in the url for the game and add "," to the second to last line like the other ones // prefered to be from unbanned games or smth so it wont be blocked, like if its emulated ig
+        // 6. Go to "button assign thing"
+        // 7. After copying and pasting a line there to make my button assign thing, replace the "get element by id" to my game id
+        // 8. On the far right replace the name to whatever i typed in for step 4
+        // 9. using the button id and other thing just like the previous step, add the open notification.
+        //
+        // also yes this file is pointless
